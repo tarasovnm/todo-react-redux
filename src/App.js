6688c2df1c2348ac6bceab52aedbdb1968b0b2cd
tarasovnm <tarasovnm@gmail.com>
2020-12-09 15:33:42 +0400

    Done markup and stylization

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="header">
+                <div className="title">
+                    <h2 className="title__date">Wednesday, December 9</h2>
+                    <p className="title__tasks">3 Active Tasks</p>
+                </div>
+                <ul className="filter">
+                    <li className="filter__item active">All tasks</li>
+                    <li className="filter__item">Incomplete</li>
+                    <li className="filter__item">Completed</li>
+                </ul>
+            </div>
+            <div className="add">
+                <input className="add__text" type="text" placeholder="Enter a task" value=""/>
+                <button className="add__btn">Add Task</button>
+            </div>
+            <ul className="tasks_list">
+                <li className="task done">
+                    <span className="task__check material-icons">check_circle_outline</span>
+                    <p className="task__text">Client meeting</p>
+                    <button className="task__del">
+                        <span className="material-icons">delete_forever</span>
+                    </button>
+                </li>
+                <li className="task">
+                    <span className="task__check material-icons">check_circle_outline</span>
+                    <p className="task__text">Design app interface</p>
+                    <button className="task__del">
+                        <span className="material-icons">delete_forever</span>
+                    </button>
+                </li>
+                <li className="task">
+                    <span className="task__check material-icons">check_circle_outline</span>
+                    <p className="task__text">Learn node.js</p>
+                    <button className="task__del">
+                        <span className="material-icons">delete_forever</span>
+                    </button>
+                </li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
